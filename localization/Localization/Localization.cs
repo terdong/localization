8536d28616a713745569831a0e2828b06b7d27c0
terdong <terdong@gmail.com>
2016-08-13 09:58:23 +0900

    1. 첫번째 커밋

---
+++ localization/Localization/Localization.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamGehem
+{
+    public class Localization
+    {
+        private static Localization instance_;
+        private string csv_file_path_;
+        private CsvParser csv_parser_;
+
+        private IList<IReadOnlyDictionary<string, string>> list_;
+        private IReadOnlyDictionary<string, string> dic_;
+        private int language_index_;
+
+        public static void ChangeLanguage(int index)
+        {
+            instance_.ChangeLanguage_(index);
+        }
+
+        public static string GetValue(string key)
+        {
+            return instance_.dic_[key];
+        }
+
+        public static Localization Create(string csv_file_path)
+        {
+            instance_ = new Localization(csv_file_path);
+            return instance_;
+        }
+
+        private Localization(string csv_file_path)
+        {
+            csv_file_path_ = csv_file_path;
+            csv_parser_ = new CsvParser();
+            language_index_ = 0;
+
+
+
+            ChangeLanguage_(language_index_);
+        }
+
+        private void ChangeLanguage_(int index)
+        {
+            language_index_ = index;
+            if (list_.Count >= index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            dic_ = list_[language_index_];
+        }
+    }
+}
---
+++ localization/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamGehem;
+namespace localization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Localization.csv";
+
+            Localization l = Localization.Create(path);
+            Localization.ChangeLanguage(1);
+
+
+
+
+            //Console.WriteLine("Hello");
+
+            //using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            //{
+            //    while (!sr.EndOfStream)
+            //    {
+            //        string s = sr.ReadLine();
+            //        string[] temp = s.Split(',');        // Split() 메서드를 이용하여 ',' 구분하여 잘라냄
+            //        if (temp.Length > 2)
+            //        {
+            //            Console.WriteLine("{0},{1},{2}", temp[0], temp[1], temp[2]);
+            //        }
+            //        else if (temp.Length > 1)
+            //        {
+            //            Console.WriteLine("{0},{1}", temp[0], temp[1]);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
