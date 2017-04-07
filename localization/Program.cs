d1b95a1e3e0ddd89943a2dfd10874c903a59e632
DongHee <terdong@gmail.com>
2017-04-07 12:41:03 +0900

    1. 이전을 위한 커밋

--- localization/Program.cs
+++ localization/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TeamGehem;
 namespace localization
 {
@@ -14,29 +10,13 @@ namespace localization
             string path = "Localization.csv";
 
             Localization l = Localization.Create(path);
+
+            Console.WriteLine(Localization.GetValue("KEY"));
+            Console.WriteLine(Localization.GetValue("Login_Message"));
             Localization.ChangeLanguage(1);
+            Console.WriteLine(Localization.GetValue("KEY"));
+            Console.WriteLine(Localization.GetValue("Login_Message"));
 
-
-
-
-            //Console.WriteLine("Hello");
-
-            //using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
-            //{
-            //    while (!sr.EndOfStream)
-            //    {
-            //        string s = sr.ReadLine();
-            //        string[] temp = s.Split(',');        // Split() 메서드를 이용하여 ',' 구분하여 잘라냄
-            //        if (temp.Length > 2)
-            //        {
-            //            Console.WriteLine("{0},{1},{2}", temp[0], temp[1], temp[2]);
-            //        }
-            //        else if (temp.Length > 1)
-            //        {
-            //            Console.WriteLine("{0},{1}", temp[0], temp[1]);
-            //        }
-            //    }
-            //}
         }
     }
 }
